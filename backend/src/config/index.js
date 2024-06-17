d74df7653e920b93d99ddeddfdc8cbdc81afe0de
GowthamJSDev <83946937+GowthamJSDev@users.noreply.github.com>
2024-06-17 16:18:32 +0530

    updated config

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -5,6 +5,6 @@ dotenv.config();
 export const config = {
     port: process.env.PORT || 5000,
     voyager: {
-        apiKey: process.env.VOYAGER_API_KEY || "Kcb7B3do0Q6Vd9FHlI2UO6q92MM8hGW37NiYk2t9",
+        apiKey: process.env.VOYAGER_API_KEY,
     }
 };
